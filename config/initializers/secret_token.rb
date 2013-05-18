0e3407812ee42d9a194d2d54104be147748583f1
Zernel <itzernel@gmail.com>
2013-05-18 09:39:21 +0800

    Init the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Anderbook::Application.config.secret_key_base = '4e2f1ab162e65cecbfb45386a59669ce9b93be48365d1f758c5c31a651260fbad91331d41d511c15aeac39f87202048b639e7b7c8d9bee10e019384f0ed4367d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Anderbook::Application.config.session_store :cookie_store, key: '_anderbook_session'
